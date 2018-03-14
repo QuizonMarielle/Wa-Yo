0ac23ce997fc7b83a135407f539ec09b6e79c871
Marielle Quizon <marquiquizon@gmail.com>
2018-03-13 23:50:11 -0500

    Career Page

---
+++ js/index.js
@@ -0,0 +1,83 @@
+window.sr = ScrollReveal();
+// Index Page
+sr.reveal('.navbar', {
+  duration: 2000,
+  origin:'top'
+});
+
+sr.reveal('.ptext', {
+  duration: 2000,
+  origin:'bottom'
+});
+
+sr.reveal('.waffle-border', {
+  duration: 3000,
+  origin:'top',
+  distance: '50px'
+});
+
+sr.reveal('.b-container', {
+  duration: 3000,
+  origin:'top'
+});
+
+sr.reveal('.product-yogurt', {
+  duration: 2000,
+  origin:'right',
+  distance: '80px'
+});
+
+sr.reveal('.content', {
+  duration: 2000,
+  origin:'left',
+  distance: '80px'
+});
+
+sr.reveal('.mapouter', {
+  duration: 2000,
+  origin:'top'
+});
+
+sr.reveal('.contact-section', {
+  duration: 2000,
+  origin:'bottom'
+});
+
+
+sr.reveal('.opening-section', {
+  duration: 2000,
+  origin:'bottom'
+});
+
+//Menu Page
+sr.reveal('.borders', {
+  duration: 2000,
+  origin:'top',
+  distance: '50px'
+});
+
+sr.reveal('.menu-container', {
+  duration: 2000,
+  origin:'bottom'
+});
+
+sr.reveal('.drinks-section', {
+  duration: 2000,
+  origin:'right',
+  distance: '100px'
+});
+
+// Career page
+
+
+sr.reveal('.cashier', {
+  duration: 2000,
+  origin:'left',
+  distance: '100px'
+});
+
+sr.reveal('.barrista', {
+  duration: 2000,
+  origin:'right',
+  distance: '100px'
+});
